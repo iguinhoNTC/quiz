e4c5d36ff9ef804620e0df5058d7aac14d7ece06
Igor Henrique Mendonca <134654479+iguinhoNTC@users.noreply.github.com>
2023-11-29 11:45:56 -0300

    quiz

---
+++ components/Temporizador.tsx
@@ -0,0 +1,25 @@
+import { CountdownCircleTimer } from 'react-countdown-circle-timer'
+import styles from '../styles/Temporizador.module.css'
+
+interface TemporizadorProps {
+    key: any
+    duracao: number
+    tempoEsgotado: () => void 
+}
+
+export default function Temporizador(props: TemporizadorProps) {
+    return (    
+        <div className={styles.temporizador}>
+            <CountdownCircleTimer 
+                duration={props.duracao}
+                size={120}
+                isPlaying
+                onComplete={props.tempoEsgotado}
+                colors={['#bce596', '#f7b801', '#ed827a']}
+                colorsTime={[10, 3, 0]}
+            >
+                {({ remainingTime}) => remainingTime}
+            </CountdownCircleTimer>
+        </div>
+    )
+}
